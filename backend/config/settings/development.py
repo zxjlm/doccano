375398d344464019551af1bd7f306c8f2afb6cdd
harumonia <zxjlm233@gmail.com>
2022-05-29 11:28:53 +0800

    :whale: white list

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -2,5 +2,6 @@ from .base import *  # noqa: F403
 
 MIDDLEWARE.append("api.middleware.RangesMiddleware")  # noqa: F405
 CORS_ORIGIN_WHITELIST = (
-"http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://localhost:3000", "http://localhost:8000")
+    "http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://localhost:3000", "http://localhost:8000",
+    "http://localhost:9004", "http://localhost:9003", "http://47.103.76.95:9003", "http://47.103.76.95:9004")
 CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,3 +1,7 @@
 from .base import *  # noqa: F401,F403
 
 DEBUG = False
+CORS_ORIGIN_WHITELIST = (
+    "http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://localhost:3000", "http://localhost:8000",
+    "http://localhost:9004", "http://localhost:9003", "http://47.103.76.95:9003", "http://47.103.76.95:9004")
+CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
